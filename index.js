b0c80704f014977c2b7a42f7314c6184567e7846
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-27 18:50:27 +0200

    wip

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // import the server and start it
-const server = require('./badpath')
+const server = require('./api/server')
 
 console.log(`web46 rules!!!!!!!!`)
 console.log(process.env.LANG) // computer stuff
