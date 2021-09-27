72b083f0d90e0874de8033aca3251a442777ff2f
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-27 18:48:29 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -22,3 +22,4 @@ server.get('/', (req, res) => {
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
+module.exports = server // export default server
