d04ed255cd76502bdf41e79d7eaedcf2f8705e87
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-27 18:49:43 +0200

    wip

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // import the server and start it
+const server = require('./api/server')
+
 console.log(`web46 rules!!!!!!!!`)
 console.log(process.env.LANG) // computer stuff
 console.log(process.argv[2]) // first arg from command line
