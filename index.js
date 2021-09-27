28fb93147ff57a027ef776cdb196de4258afd700
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-27 18:34:16 +0200

    wip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // import the server and start it
 console.log(`web46 rules!!!!!!!!`)
-console.log(process.env.LANG)
-console.log(process.argv[2])
+console.log(process.env.LANG) // computer stuff
+console.log(process.argv[2]) // first arg from command line
