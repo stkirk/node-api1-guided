68fbdc146a06e3e4710ab9ae96ec965a2b3fdc4c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-27 19:43:45 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -28,8 +28,9 @@ server.get('/api/dogs/:id', async (req, res) => {
       res.status(404).json({
         message: `dog with id ${id} does not exist!`
       })
+    } else {
+      res.status(200).json(dog)
     }
-    res.status(200).json(dog)
   } catch (err) {
     res.status(500).json({
       message: err.message,
